0b94366946b79694700b80654cdbd4755391a2ea
sailendraprasath <sailendraprasath05@gmail.com>
2024-06-09 23:48:40 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-red-400 text-center ">
+        <h1>Welcome </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
